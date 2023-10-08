a5db3a1f653fb678d037e7ceeb529dbaf1f22c38
Olena Bekhtir <elena.behtir@gmail.com>
2023-10-08 19:20:22 +0300

    change format images

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,9 +7,9 @@ export const Footer = () => {
             <div class='footer'>
                 <div class='author'><span className='author-name'>Освітній проект з фізики </span> &copy; Olena Bekhtir, 2023</div>
                 <div>
-                    <a class="email" href="mailto:elena.behtir@gmail.com"><img src='./src/img/icon-email.png' alt='email' className='icons-img' /></a>
-                    <a href="https://t.me/optikB"><img src='./src/img/icon-telegram.png' alt='email' className='icons-img' /></a>
-                    <a href="https://msng.link/o?380972234161=vi"><img src='./src/img/icon-viber.png' alt='email' className='icons-img' /></a>
+                    <a class="email" href="mailto:elena.behtir@gmail.com"><img src='./src/img/icon-email.jpg' alt='email' className='icons-img' /></a>
+                    <a href="https://t.me/optikB"><img src='./src/img/icon-telegram.jpg' alt='telegram' className='icons-img' /></a>
+                    <a href="https://msng.link/o?380972234161=vi"><img src='./src/img/icon-viber.jpg' alt='viber' className='icons-img' /></a>
                 </div>
             </div>
         </>
