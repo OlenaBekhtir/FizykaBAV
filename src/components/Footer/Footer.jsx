08047f254e2cb3f60433a1c2fa43a395648e980b
Olena Bekhtir <elena.behtir@gmail.com>
2023-10-08 19:30:09 +0300

    change path icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './footer.css';
+import Email from './../../img/icon-email.jpg';
+import Telegram from './../../img/icon-telegram.jpg';
+import Viber from './../../img/icon-viber.jpg';
 
 export const Footer = () => {
     return (
@@ -7,9 +10,9 @@ export const Footer = () => {
             <div class='footer'>
                 <div class='author'><span className='author-name'>Освітній проект з фізики </span> &copy; Olena Bekhtir, 2023</div>
                 <div>
-                    <a class="email" href="mailto:elena.behtir@gmail.com"><img src='./src/img/icon-email.jpg' alt='email' className='icons-img' /></a>
-                    <a href="https://t.me/optikB"><img src='./src/img/icon-telegram.jpg' alt='telegram' className='icons-img' /></a>
-                    <a href="https://msng.link/o?380972234161=vi"><img src='./src/img/icon-viber.jpg' alt='viber' className='icons-img' /></a>
+                    <a class="email" href="mailto:elena.behtir@gmail.com"><img src={Email} alt='email' className='icons-img' /></a>
+                    <a href="https://t.me/optikB"><img src={Telegram} alt='telegram' className='icons-img' /></a>
+                    <a href="https://msng.link/o?380972234161=vi"><img src={Viber} alt='viber' className='icons-img' /></a>
                 </div>
             </div>
         </>
