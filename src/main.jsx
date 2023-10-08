8e294e8cb1445981746d7ff3ecb0c58637945a21
Olena Bekhtir <elena.behtir@gmail.com>
2023-10-08 19:01:44 +0300

    add some changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
   HashRouter as Router
-} from "react-router-dom";
+} from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 
