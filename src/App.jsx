5a60e7bb047ca5653dc68ae4d1beebe2258df53f
Olena Bekhtir <elena.behtir@gmail.com>
2024-03-31 20:15:49 +0300

    add adaptove for header and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
       <div className='main'>
 
         <Header/> 
-        <Sidebar /> 
+        <Sidebar/> 
 
         <Routes>
           {/* {...routes.map(r => <Route {...r} />)} */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,10 @@ import Logo from './../../img/logo.webp';
 import './header.css';
 import { Link } from 'react-router-dom';
 
-export const Header = ({ showSidebar, toggleSidebar }) => {
+export const Header = ({ showMenu, toggleMenu }) => {
   return (
     <>
+    <div className='header'>
       <div className='logo-wrap'>
         <div className='wrap-img'>
           <Link to={`/`}>
@@ -13,17 +14,14 @@ export const Header = ({ showSidebar, toggleSidebar }) => {
           </Link>
         </div>
         <h1 className='title'>FizykaBAV</h1>
+      </div>
 
-        {/* В залежності від значення showSidebar, міняємо класи кнопки */}
-        {showSidebar ? (
-          <button className='toggle-button' onClick={toggleSidebar}>
+          <button className='burger-button' onClick={toggleMenu}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
               <path d="M0 0h24v24H0z" fill="none" />
               <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
             </svg>
           </button>
-        ) : null}
-
       </div>
     </>
   )
